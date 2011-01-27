616bde8498e3be2ce8ceeb79675e78680cc065ef
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-01-26 19:58:00 -0600

    clicking support is in but it doesn't seem to work

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var width, height;
+// globals
+var gCanvas, gDrawingContext, gSpriteDict;
 
 // Run initGame() after the page finishes loading.
 window.addEventListener("load", initGame, false);
@@ -11,20 +12,20 @@ function initGame() {
         return;
     }
     // get drawing context from main canvas
-    var canvas = document.getElementById("gameCanvas");
-    var gContext = canvas.getContext("2d");
-    width = canvas.width;
-    height = canvas.height;
+    gCanvas = document.getElementById("gameCanvas");
+    gDrawingContext = gCanvas.getContext("2d");
     // get image objects ready
     var imageNames = ["bob"];
     var imageDict = loadImages(imageNames);
     // make sprites
-    var spriteDict = new Array();
-    spriteDict["bob"] = new Sprite(imageDict["bob"], 60, 60);
+    gSpriteDict = new Array();
+    gSpriteDict["bob"] = new Sprite(imageDict["bob"], [60, 60], [1, 2]);
     // register event handlers
-
+    gCanvas.addEventListener("keydown", handleKeyDown, false);
+    gCanvas.addEventListener("keyup", handleKeyUp, false);
+    gCanvas.addEventListener("click", handleClick, false);
     // initiate game loop
-    window.setInterval(updateGame, 20, spriteDict, gContext);
+    window.setInterval(updateGame, 20);
 }
 
 // Take an array of image names; load them from images/ (assuming .png type).
@@ -40,17 +41,49 @@ function loadImages(imageNames) {
 }
 
 // Main game loop body.
-function updateGame(spriteDict, gContext) {
-    for (var name in spriteDict) {
-        spriteDict[name].update();
+function updateGame() {
+    for (var name in gSpriteDict) {
+        gSpriteDict[name].update();
     }
-    drawGame(spriteDict, gContext);
+    drawGame();
 }
 
 // Draw everything.
-function drawGame(spriteDict, gContext) {
-    gContext.fillRect(0, 0, width, height);
-    for (var name in spriteDict) {
-        spriteDict[name].draw(gContext);
+function drawGame() {
+    gDrawingContext.fillRect(0, 0, gCanvas.width, gCanvas.height);
+    for (var name in gSpriteDict) {
+        gSpriteDict[name].draw(gDrawingContext);
     }
+}
+
+function handleKeyDown(keyEvent) {
+
+}
+
+function handleKeyUp(keyEvent) {
+
+}
+
+function handleClick(clickEvent) {
+    var position = getCursorPosition(clickEvent);
+    alert(position[0] + ' ' + position[1]);
+}
+
+// Code pulled straight from Dive Into HTML5 (Halma)
+// Unsure which browsers don't support pageX/pageY.
+function getCursorPosition(clickEvent) {
+    var x, y;
+    if (e.pageX != undefined && e.pageY != undefined) {
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else {
+        x = e.clientX + document.body.scrollLeft 
+                + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop 
+                + document.documentElement.scrollTop;
+    }
+    x -= gCanvas.offsetLeft;
+    y -= gCanvas.offsetTop;
+    return [x, y];
 }
--- sprite.js
+++ sprite.js
@@ -1,11 +1,14 @@
-function Sprite(image, initX, initY) {
+function Sprite(image, position, velocity) {
     this.image = image;
-    this.posX = initX;
-    this.posY = initY;
+    this.posX = position[0];
+    this.posY = position[1];
+    this.speedX = velocity[0];
+    this.speedY = velocity[1];
     this.update = function () {
-
+        this.posX += this.speedX;
+        this.posY += this.speedY;
     };
-    this.draw = function (gDrawingContext) {
-        gDrawingContext.drawImage(this.image, this.posX, this.posY);
+    this.draw = function (drawingContext) {
+        drawingContext.drawImage(this.image, this.posX, this.posY);
     };
 }
